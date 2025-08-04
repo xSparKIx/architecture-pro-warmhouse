7a5a0698c0c1616e3a57a6aa3c678e77a9592720
SparKI <sparki.projects@yandex.ru>
2025-08-04 22:00:00 +0300

    Исправление строки подключения

--- apps/smart_home/main.go
+++ apps/smart_home/main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	// Set up database connection
-	dbURL := getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:8082/smarthome")
+	dbURL := getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/smarthome")
 	database, err := db.New(dbURL)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
