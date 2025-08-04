d331ed50a6313da95c38076bdf08568494641bd7
SparKI <sparki.projects@yandex.ru>
2025-08-05 00:19:10 +0300

    Обновление задания 5

--- apps/temperature-api/src/app.controller.ts
+++ apps/temperature-api/src/app.controller.ts
@@ -9,11 +9,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/temperature')
-  getTemperature(@Query('location') location: string): number {
-    if (!location) {
-      throw new BadRequestException('Не передана локация');
+  getTemperature(@Query('location') location: string, @Query('sensor_id') sensor_id: string): any {
+    if (!location && !sensor_id) {
+      throw new BadRequestException('Не передана локация и датчик');
     }
 
-    return this.appService.getRandomTemperature(location);
+    return this.appService.getRandomTemperature(location, sensor_id);
   }
 }
--- apps/temperature-api/src/app.service.ts
+++ apps/temperature-api/src/app.service.ts
@@ -2,16 +2,51 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getRandomTemperature(location: string): any {
+  private getLocationBySensorId(sensor_id: string): string {
+    const locations = {
+      "1": "Living Room",
+      "2": "Bedroom",
+      "3": "Kitchen",
+		}
+
+    return locations[sensor_id] || "Unknown";
+  }
+
+  private getSensorIdByLocation(location: string): string {
+    const ids = {
+      "Living Room": "1",
+      Bedroom: "2",
+      Kitchen: "3",
+    }
+
+    return ids[location] || "0";
+  }
+
+  getRandomTemperature(location: string, sensor_id: string): any {
+    if (!location && !sensor_id) {
+      return {};
+    }
+
+    let currentLocation = location;
+    let sensorId = sensor_id;
+
+    if (!currentLocation) {
+      currentLocation = this.getLocationBySensorId(sensorId);
+    }
+
+    if (!sensorId) {
+      sensorId = this.getSensorIdByLocation(location);
+    }
+
     return {
-      location,
+      location: currentLocation,
       value: Math.ceil(Math.random() * (30 - -30) + -30),
       unit: 'C',
       status: Math.random() > 0.5 ? 'active' : 'deactivated',
       timestamp: new Date(),
-      description: `Информация о локации ${location}`,
-      sensor_id: Math.ceil(Math.random() * (100000 - 999999) + 999999).toString(),
-      sensor_type: 'Тестовый датчик'
+      description: `Информация о локации ${currentLocation}`,
+      sensor_id: sensorId,
+      sensor_type: `Датчик №${sensorId}`
     }
   }
 }
